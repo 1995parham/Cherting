9fdee94e3cbb36b68d544c5a6921958e1ca5af2e
Parham Alvani <parham.alvani@gmail.com>
2015-11-30 22:57:02 +0330

    Current source codes were added.

---
+++ cherting/src/main/java/home/parham/cherting/AppComponent.java
@@ -0,0 +1,91 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : AppComponent.java
+ *
+ * [] Creation Date : 06-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/**
+ * @author Parham Alvani
+*/
+/*
+ * Copyright 2014 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package home.parham.cherting;
+
+import org.apache.felix.scr.annotations.*;
+import org.onlab.packet.Ethernet;
+import org.onosproject.app.ApplicationService;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.host.HostService;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * SDVPN application component.
+ */
+@Component(immediate = true)
+public class AppComponent {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected HostService hostService;
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected PacketService packetService;
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected ApplicationService applicationService;
+
+	@Activate
+	protected void activate() {
+		ApplicationId chertId = applicationService.getId("home.parham.cherting");
+		L2SwitchingHandler handler = new L2SwitchingHandler(hostService, packetService);
+
+		/*
+		 * Adds the specified processor to the list of packet processors.
+		 * It will be added into the list in the order of priority.
+		 * The higher numbers will be processing the packets after the lower numbers.
+		 * Parameters:
+		 * processor - processor to be added
+		 * priority - priority in the reverse natural order
+		 */
+		packetService.addProcessor(handler, PacketProcessor.director(2));
+		TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+		selector.matchEthType(Ethernet.TYPE_IPV4);
+		/*
+		 * Priorities available to applications for requests for packets from the data plane.
+		 * [] CONTROL: High priority for control traffic.
+		 * [] REACTIVE: Low priority for reactive applications.
+		 */
+		packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, chertId);
+
+		log.info("Started");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		log.info("Stopped");
+	}
+}
---
+++ cherting/src/main/java/home/parham/cherting/L2SwitchingHandler.java
@@ -0,0 +1,102 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : L2SwitchingHandler.java
+ *
+ * [] Creation Date : 06-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/**
+ * @author Parham Alvani
+ */
+package home.parham.cherting;
+
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.MacAddress;
+import org.onosproject.net.Host;
+import org.onosproject.net.HostId;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.host.HostService;
+import org.onosproject.net.packet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class L2SwitchingHandler implements PacketProcessor {
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	private PacketService packetService;
+	private HostService hostService;
+
+	public L2SwitchingHandler(HostService hostService, PacketService packetService) {
+		this.hostService = hostService;
+		this.packetService = packetService;
+	}
+
+	@Override
+	public void process(PacketContext context) {
+		/*
+		 * Stop processing if the packet has been handled, since we
+		 * can't do any more to it.
+		*/
+		if (context.isHandled()) {
+			return;
+		}
+		InboundPacket pkt = context.inPacket();
+		Ethernet ethPkt = pkt.parsed();
+
+		if (ethPkt == null) {
+			return;
+		}
+
+		if (ethPkt.getDestinationMAC().equals(MacAddress.BROADCAST)) {
+			flood(context);
+			return;
+		}
+
+		HostId dstId = HostId.hostId(ethPkt.getDestinationMAC());
+
+		/*
+		 * Do we know who this is for? If not, flood and bail.
+		 */
+		Host dst = hostService.getHost(dstId);
+		if (dst == null) {
+			flood(context);
+			return;
+		}
+
+		if (dst.vlan().toShort() == ethPkt.getVlanID()) {
+			forwardPacketToDst(context, dst);
+		}
+	}
+
+	/*
+	 * Forward packet into destination switch directly :)) how fun
+	 */
+	private void forwardPacketToDst(PacketContext context, Host dst) {
+		TrafficTreatment treatment = DefaultTrafficTreatment.builder().setOutput(dst.location().port()).build();
+		OutboundPacket packet = new DefaultOutboundPacket(dst.location().deviceId(),
+			treatment, context.inPacket().unparsed());
+		packetService.emit(packet);
+		log.info("sending packet: {}", packet);
+	}
+
+
+	/*
+	 * Floods the specified packet if permissible.
+	 */
+	private void flood(PacketContext context) {
+		packetOut(context, PortNumber.FLOOD);
+	}
+
+	/*
+	 * Sends a packet out the specified port.
+	 */
+	private void packetOut(PacketContext context, PortNumber portNumber) {
+		context.treatmentBuilder().setOutput(portNumber);
+		context.send();
+	}
+
+}
